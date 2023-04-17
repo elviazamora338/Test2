2c307fabe8d49a7e42c5b43c197affbbf80a51e1
heb479 <ana.garcia30@utrgv.edu>
2023-04-17 17:37:58 -0500

    new travel log (still working) - Ana

---
+++ SeekAdventure2.2 (1)/SeekAdventureProject-master/app/src/main/java/com/example/myapplication/Fragments/CreateJournalFragment.java
@@ -0,0 +1,42 @@
+package com.example.myapplication.Fragments;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.UploadActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class CreateJournalFragment extends Fragment {
+
+    public CreateJournalFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_journal, container, false);
+
+        // Button
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), UploadActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- SeekAdventure2.2 (1)/SeekAdventureProject-master/app/src/main/java/com/example/myapplication/MainMenu.java
+++ SeekAdventure2.2 (1)/SeekAdventureProject-master/app/src/main/java/com/example/myapplication/MainMenu.java
@@ -1,31 +1,44 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.app.ActionBar;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.Toast;
-
-import androidx.appcompat.widget.Toolbar;
+import com.example.myapplication.Fragments.AboutFragment;
+import com.example.myapplication.Fragments.CreateJournalFragment;
+import com.example.myapplication.Fragments.FavoritesFragment;
+import com.example.myapplication.Fragments.HomeFragment;
+import com.example.myapplication.Fragments.LocationFragment;
+import com.example.myapplication.Fragments.SettingsFragment;
+import com.example.myapplication.Fragments.ShareFragment;
 import com.example.myapplication.databinding.ActivityMainMenuBinding;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.gms.common.annotation.NonNullApi;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.Calendar;
 
 public class MainMenu extends AppCompatActivity {
 
     ActivityMainMenuBinding binding;
-    FloatingActionButton fab;
     DrawerLayout drawerLayout;
+
+    // Calendar Variables
+    TextView tvDate;
+    EditText etDate;
+    DatePickerDialog.OnDateSetListener setListener;
 
 
     @Override
@@ -75,9 +88,55 @@ public class MainMenu extends AppCompatActivity {
         });
 
 
+        // Creates calendar view on "Create Journal"
+//        tvDate = findViewById(R.id.tv_date);
+        etDate = findViewById(R.id.et_date);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+//
+//        tvDate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(
+//                        MainMenu.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
+//                        , setListener,year,month,day);
+//                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                datePickerDialog.show();
+//            }
+//        });
+//
+//        setListener = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                month = month+1;
+//                String date = day+"/"+month+"/"+year;
+//                tvDate.setText(date);
+//            }
+//        };
+//
+//        etDate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(
+//                        MainMenu.this, new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+//                        month = month+1;
+//                        String date = day+"/"+month+"/"+year;
+//                        etDate.setText(date);
+//                    }
+//                },year,month,day);
+//                datePickerDialog.show();
+//            }
+//        });
+
+
     }
     // Fragments for navigation drawer (left-side)
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_home:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
@@ -116,7 +175,6 @@ public class MainMenu extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_layout,fragment);
         fragmentTransaction.commit();
     }
-
 
 
 }
